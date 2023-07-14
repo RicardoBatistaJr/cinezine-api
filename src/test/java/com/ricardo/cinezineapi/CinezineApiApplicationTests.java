184d64d607bf5b4e5aac174e2369a5750ca76192
RicardoBatistaJr <ricardowalker01.rw@gmail.com>
2023-07-13 22:22:11 -0300

    Primeiro commit e estrutura inicial do projeto

---
+++ src/test/java/com/ricardo/cinezineapi/CinezineApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ricardo.cinezineapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinezineApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
